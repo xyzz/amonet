f914e03d3e17881baa6761641f21affa80f927bf
Ilya Zhuravlev <whatever@xyz.is>
2019-06-29 21:51:30 -0400

    fastboot-step: do normal reboot so it's compatible with older fastboots

--- fastboot-step.sh
+++ fastboot-step.sh
@@ -4,9 +4,9 @@ set -e
 
 fastboot flash boot bin/recovery-inj.img
 fastboot flash recovery bin/recovery-inj.img
-fastboot reboot recovery
+fastboot reboot
 
 echo ""
 echo ""
-echo "If you don't see the recovery in a few seconds, try pressing the power button twice"
+echo "If you don't see the recovery in a few seconds, press the power button twice"
 echo ""
